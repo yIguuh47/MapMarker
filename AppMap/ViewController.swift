35d90077085e6dc0488b57f812981f025f05b578
yIguuh47 <epic.47.54.19@gmail.com>
2022-06-14 11:46:51 -0300

    Initial Commit

---
+++ AppMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMap
+//
+//  Created by Virtual Machine on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
